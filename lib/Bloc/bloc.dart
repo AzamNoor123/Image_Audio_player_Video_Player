2c71b7be2330acfcdf8aad734a3f85ccac008ec6
azamn <azam.noor@techswevil.com>
2023-05-03 19:44:09 +0500

    task done

--- lib/Bloc/bloc.dart
+++
@@ -1,3 +0,0 @@
-export 'imagebloc_bloc.dart';
-export 'imagebloc_event.dart';
-export 'imagebloc_state.dart';
--- lib/Bloc/imagebloc_event.dart
+++
@@ -1,5 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class ImageBlocEvent extends Equatable {
-  const ImageBlocEvent();
-}
